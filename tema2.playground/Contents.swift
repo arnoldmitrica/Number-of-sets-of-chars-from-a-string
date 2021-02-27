f5cf8e9da0b219411bb662178846e4a260264c8c
Arnold Mitrica <62401775+arnoldmitrica@users.noreply.github.com>
2021-02-28 01:59:06 +0200

    Update Contents.swift

--- tema2.playground/Contents.swift
+++ tema2.playground/Contents.swift
@@ -30,5 +30,21 @@ func maximumNumberOfMoves(sampleWord wordu:String, appliedTo stru: String) -> An
 }
 
 print(maximumNumberOfMoves(sampleWord: "APPLE", appliedTo: "ELPPAELPPAAAAAAA"))
-//animals.filter { $0 != "chimps" }
-//let nr = str.cont
+
+//SIMPLIFIED VERSION FOR BETTER UNDERSTANDING
+
+// var str = "ELPPAELPPAELPPAELPPA"
+
+// var word = "APPLE"
+
+// var setWord = Set<Character>()
+
+// var squeezedWord = word.filter{ setWord.insert($0).inserted }
+
+// var duplicatesArrayWord = setWord.sorted().map { x in word.filter { $0 == x}.count }.reversed() as [Int]
+
+// var setWordSorted = setWord.sorted().reversed() as [Character]
+
+// var duplicatesarrayStr = setWordSorted.map { x in str.filter { $0 == x}.count }.filter { $0 != 0 } as [Int]
+
+// print(zip(duplicatesarrayWord,duplicatesarrayStr).map{$1 / $0}.min() ?? "Nil unrwapped" as Any)
