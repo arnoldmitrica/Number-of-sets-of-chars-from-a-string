24d712519424bf8c9e0d2005ce67597659db6b5d
Arnold Mitrica <62401775+arnoldmitrica@users.noreply.github.com>
2021-02-28 02:01:22 +0200

    Update Contents.swift

--- tema2.playground/Contents.swift
+++ tema2.playground/Contents.swift
@@ -1,25 +1,4 @@
-import UIKit
-
-var str = "ELPPAELPPAELPPAELPPA"
-
-var word = "APPLE"
-
-var dict = [Character:Int]()
-
-var setWord = Set<Character>()
-var setStr = Set<Character>()
-
-
-var squeezedWord = word.filter{ setWord.insert($0).inserted }
-
-var duplicatesarrayWord = setWord.sorted().map { x in word.filter { $0 == x}.count }.reversed() as [Int]
-
-var setWordSorted = setWord.sorted().reversed() as [Character]
-
-var duplicatesarrayStr = setWordSorted.map { x in str.filter { $0 == x}.count }.filter { $0 != 0 } as [Int]
-
-print(zip(duplicatesarrayWord,duplicatesarrayStr).map{$1 / $0}.min() ?? "Nil unrwapped" as Any)
-
+###
 
 func maximumNumberOfMoves(sampleWord wordu:String, appliedTo stru: String) -> Any {
 
