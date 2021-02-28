e228acbb8f8efd85622bc2a8bfa9515fbbb47223
Arnold Mitrica <62401775+arnoldmitrica@users.noreply.github.com>
2021-02-28 11:46:44 +0200

    Update Contents.swift

--- tema2.playground/Contents.swift
+++ tema2.playground/Contents.swift
@@ -1,10 +1,10 @@
 ###
 
-func maximumNumberOfMoves(sampleWord wordu:String, appliedTo stru: String) -> Any {
+func maximumNumberOfMoves(sampleWord word:String, appliedTo str: String) -> Any {
 
     var setWord = Set<Character>()
 
-    return zip(wordu.filter{ setWord.insert($0).inserted }.sorted().map { x in wordu.filter{ $0 == x}.count }.reversed() as [Int], (setWord.sorted().reversed() as [Character]).map { x in stru.filter { $0 == x}.count }.filter { $0 != 0 } as [Int]).map{$1/$0}.min() ?? "Nil unrwapped" as Any
+    return zip(word.filter{ setWord.insert($0).inserted }.sorted().map { x in word.filter{ $0 == x}.count }.reversed() as [Int], (setWord.sorted().reversed() as [Character]).map { x in str.filter { $0 == x}.count }.filter { $0 != 0 } as [Int]).map{$1/$0}.min() ?? "Nil unrwapped" as Any
 
 }
 
